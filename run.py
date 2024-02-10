64d432a011d4b0f1e6d814c6f94bbc262e3fd516
paddyw11 <>
2024-02-10 22:58:07 +0000

    Create get sales data function to get data string from user

--- run.py
+++ run.py
@@ -14,9 +14,16 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
 
-print(data)
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_sales_data():
+    """
+    Get sales figues input from the owner
+    """
+    print('Please enter sales data from the last market')
+    print("Data should be six numbers, seperated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+    
+get_sales_data()
